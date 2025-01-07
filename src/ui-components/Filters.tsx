14ec51c1fe275dc0af3f065a161885e786d8d0ad
raguzf91 <raguzf91@gmail.com>
2025-01-07 15:41:10 +0100

    finished with slider and filter components

--- src/ui-components/Filters.tsx
+++ src/ui-components/Filters.tsx
@@ -1,17 +1,84 @@
-import { useState } from "react";
-import { IoMdClose } from "react-icons/io";
-import Slider from "./Slider";
+import React, { useState } from 'react';
+import { IoMdClose } from 'react-icons/io';
+import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci';
+import Slider from './Slider';
+import { FaWifi } from "react-icons/fa";
+import { TbToolsKitchen } from "react-icons/tb";
+import { FaRegSnowflake } from "react-icons/fa";
+import { FaTv } from "react-icons/fa";
+import { LuWashingMachine } from "react-icons/lu";
+import { BiSolidDryer } from "react-icons/bi";
+import { TbTemperatureSun } from "react-icons/tb";
+import { PiCity } from "react-icons/pi";
+import { FaUmbrellaBeach } from "react-icons/fa";
+import { FaSkiing } from "react-icons/fa";
+import { LuHouse } from "react-icons/lu";
+import { PiBuildingApartment } from "react-icons/pi";
+import { IoPeopleOutline } from "react-icons/io5";
+
 const Filters = () => {
     const [activeButton, setActiveButton] = useState<string | null>(null);
+    const [rangeValue, setRangeValue] = useState<{ min: number; max: number }>({ min: 0, max: 1000 });
+    const [spavaceSobe, setSpavaceSobe] = useState(0);
+    const [kreveti, setKreveti] = useState(0);
+    const [kupaonice, setKupaonice] = useState(0);
 
     const handleButtonClick = (buttonType: string) => {
         setActiveButton(buttonType);
     };
 
-    const [rangeValue, setRangeValue] = useState<{min: number, max: number}>({min: 0, max: 1000});
     const handleRangeChange = (min: number, max: number) => {
-        setRangeValue({min, max});
+        setRangeValue({ min, max });
     };
+
+    const handleSobeKrevetiMinus = (index: number) => {
+        if (index === 0 && spavaceSobe > 0) {
+            setSpavaceSobe(spavaceSobe - 1);
+        } else if (index === 1 && kreveti > 0) {
+            setKreveti(kreveti - 1);
+        } else if (index === 2 && kupaonice > 0) {
+            setKupaonice(kupaonice - 1);
+        }
+    };
+
+    const handleSobeKrevetiAdd = (index: number) => {
+        if (index === 0) {
+            setSpavaceSobe(spavaceSobe + 1);
+        } else if (index === 1) {
+            setKreveti(kreveti + 1);
+        } else if (index === 2) {
+            setKupaonice(kupaonice + 1);
+        }
+    };
+
+    const sobeKreveti = [
+        { item: 'Spavaće sobe', value: spavaceSobe },
+        { item: 'Kreveti', value: kreveti },
+        { item: 'Kupaonice', value: kupaonice },
+    ];
+
+    const znacajke = [
+        {icon: <FaWifi />, text: 'Wi-Fi'},
+        {icon: <TbToolsKitchen />, text: 'Kuhinja'},
+        {icon: <FaRegSnowflake />, text: 'Klima-uređaj'},
+        {icon: <FaTv />, text: 'TV'},
+        {icon: <LuWashingMachine />, text: 'Perilica'},
+        {icon: <BiSolidDryer />, text: 'Sušilica'},
+        {icon: <TbTemperatureSun />, text: 'Grijanje'}
+
+    ];
+
+    const lokacije = [
+        {icon: <PiCity /> , text: 'Centar grada'},
+        {icon: <FaUmbrellaBeach />, text: 'Plaža'},
+        {icon: <FaSkiing /> , text: 'Skijalište'},
+    ];
+
+    const vrsteSmjestaja = [
+        {icon: <LuHouse />, text: 'Kuća'},
+        {icon: <PiBuildingApartment />, text: 'Stan'},
+        {icon: <IoPeopleOutline />, text: 'Dijeljenje stana'},
+    ];
 
     return (
         <div className="filter-container flex-col w-full justify-center items-center">
@@ -26,52 +93,111 @@ const Filters = () => {
             <div className="vrste-smjestaja m-4 flex-col justify-center items-center border-b-2 pb-8">
                 <h2 className="text-xl h-12 font-bold mb-4">Vrste smještaja</h2>
                 <div className="flex items-center justify-between rounded-3xl border-2 p-2">
-                    
-                <button
-                    className={`text-lg font-semibold text-center rounded-3xl w-1/3 h-12 transition-all duration-100 ${
-                        activeButton === 'Sve vrste'
-                            ? 'border-slate-950 border-2'
-                            : 'hover:bg-slate-100'
-                    }`}
-                    onClick={() => handleButtonClick('Sve vrste')}
-                >
-                    Sve vrste
-                </button>
-                   
-                    
-                <button
-                    className={`text-lg font-semibold text-center rounded-3xl w-1/3 h-12 transition-all duration-100 ${
-                        activeButton === 'Soba'
-                            ? 'border-slate-950 border-2'
-                            : 'hover:bg-slate-100'
-                    }`}
-                    onClick={() => handleButtonClick('Soba')}
-                >
-                    Soba
-                </button>
-                <button
-                    className={`text-lg font-semibold text-center rounded-3xl w-1/3 h-12 transition-all duration-100 ${
-                        activeButton === 'Cijeli prostor'
-                            ? 'border-slate-950 border-2'
-                            : 'hover:bg-slate-100'
-                    }`}
-                    onClick={() => handleButtonClick('Cijeli prostor')}
-                >
-                    Cijeli prostor
-                </button>    
+                    <button
+                        className={`text-lg font-semibold text-center rounded-3xl w-1/3 h-12 transition-all duration-100 ${
+                            activeButton === 'Sve vrste'
+                                ? 'border-slate-950 border-2'
+                                : 'hover:bg-slate-100'
+                        }`}
+                        onClick={() => handleButtonClick('Sve vrste')}
+                    >
+                        Sve vrste
+                    </button>
+                    <button
+                        className={`text-lg font-semibold text-center rounded-3xl w-1/3 h-12 transition-all duration-100 ${
+                            activeButton === 'Soba'
+                                ? 'border-slate-950 border-2'
+                                : 'hover:bg-slate-100'
+                        }`}
+                        onClick={() => handleButtonClick('Soba')}
+                    >
+                        Soba
+                    </button>
+                    <button
+                        className={`text-lg font-semibold text-center rounded-3xl w-1/3 h-12 transition-all duration-100 ${
+                            activeButton === 'Cijeli prostor'
+                                ? 'border-slate-950 border-2'
+                                : 'hover:bg-slate-100'
+                        }`}
+                        onClick={() => handleButtonClick('Cijeli prostor')}
+                    >
+                        Cijeli prostor
+                    </button>
                 </div>
             </div>
             <div className="raspon-cijena m-4 flex-col justify-center items-center border-b-2 pb-8">
                 <h2 className="text-xl h-12 font-bold">Raspon cijena</h2>
-                <h3 className="text-base ">Cijena noćenja uključujući naknade i poreze</h3>
+                <h3 className="text-base">Cijena noćenja uključujući naknade i poreze</h3>
                 <div>
-                    <Slider min={0} max={1000} currencyText="€" onChange={() => {
-                       handleRangeChange;
+                    <Slider min={rangeValue.min} max={rangeValue.max} currencyText="€" onChange={() => handleRangeChange} />
+                </div>
+            </div>
+            <div className="sobe-kreveti m-4 flex-col justify-center items-center border-b-2 pb-8">
+                <h2 className="text-xl h-12 font-bold">Sobe i kreveti</h2>
+                {sobeKreveti.map((item, index) => {
+                    return (
+                        <div key={index} className="flex justify-between items-center ml-4 mr-4">
+                            <p className="text-lg font-semibold">{item.item}</p>
+                            <div className="flex gap-4 justify-center items-center">
+                                <CiCircleMinus onClick={() => handleSobeKrevetiMinus(index)} className={`w-8 h-8 cursor-pointer ${item.value === 0 ? 'text-gray-400 cursor-not-allowed' : 'hover:border-2 hover:border-slate-950'}`} />
+                                <p className="text-lg">{item.value}</p>
+                                <CiCirclePlus onClick={() => handleSobeKrevetiAdd(index)} className="w-8 h-8 cursor-pointer hover:border-2 hover:border-slate-950" />
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='sadrzaji m-4 flex-col justify-center items-center border-b-2 pb-8'>
+                <h2 className='text-xl h-12 font-bold'>Sadržaji</h2>
+                <h3 className="text-lg font-bold mb-2">Osnovni sadržaji</h3>
+                <div className='grid grid-cols-4 gap-2'>
+                        {znacajke.map((znacajka, index) => {
+                            return (
+                                <div key={index} className='flex justify-center items-center rounded-3xl border-2 w-40 p-2 hover:border-slate-950 transition-all duration-100 cursor-pointer gap-y-4 gap-x-2'>
+                                    <div className='  flex justify-center items-center'>
+                                        {React.cloneElement(znacajka.icon, { className: 'w-8 h-8' })}
+                                    </div>
+                                    <p className='text-lg '>{znacajka.text}</p>
+                                </div>
+                            );
+                        })}
+                    
+                </div>
+                <div className='lokacija mt-4 border-b-2 border-b-slate-400 pb-8'>
+                    <h2 className='text-xl h-12 font-bold'>Lokacije</h2>
+                    <div className='flex items-center gap-4'>
+                    {lokacije.map((lokacija, index) => {
+                        return (
+                            <div key={index} className='flex justify-center  items-center rounded-3xl border-2 w-40 p-2 hover:border-slate-950 transition-all duration-100 cursor-pointer gap-y-4 gap-4'>
+                                <div className='  flex justify-center items-center'>
+                                    {React.cloneElement(lokacija.icon, { className: 'w-8 h-8' })}
+                                </div>
+                                <p className='text-lg '>{lokacija.text}</p>
+                            </div>
+                        );
+                    })}
 
-                    }} />
+                    </div>
+                    
+                </div>
+
+                <div className='vrsta-smjestaja mt-4 border-b-2 border-b-slate-400 pb-8'>
+                <h2 className='text-xl h-12 font-bold'>Vrste smještaja</h2>
+                    <div className='flex items-center gap-4'>
+                    {vrsteSmjestaja.map((vrsteSmjestaja, index) => {
+                        return (
+                            <div key={index} className='flex justify-center  items-center rounded-3xl border-2 w-40 p-2 hover:border-slate-950 transition-all duration-100 cursor-pointer gap-y-4 gap-4'>
+                                <div className='  flex justify-center items-center'>
+                                    {React.cloneElement(vrsteSmjestaja.icon, { className: 'w-8 h-8' })}
+                                </div>
+                                <p className='text-lg '>{vrsteSmjestaja.text}</p>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
+    </div>
     );
 };
 
--- src/ui-components/Slider.tsx
+++ src/ui-components/Slider.tsx
@@ -1,156 +1,151 @@
-import React, {
-    useCallback,
-    useEffect,
-    useState,
-    useRef
-} from 'react';
-
-const valueCSS = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 1,
-    gap: "2px",
-    paddingTop: "10px",
-};
+import React, { useState, useRef, useEffect } from 'react';
+import './css/Slider.css';
 
 interface SliderProps {
     min: number;
     max: number;
-    trackColor?: string;
     onChange: (value: { min: number; max: number }) => void;
-    rangeColor?: string;
-    valueStyle?: React.CSSProperties;
-    width?: string;
     currencyText?: string;
 }
 
 const Slider: React.FC<SliderProps> = ({
     min,
     max,
-    trackColor = "#cecece",
     onChange,
-    rangeColor = "#ff0303",
-    valueStyle = valueCSS,
-    width = "300px",
     currencyText = "$",
 }) => {
 
-    const [minVal, setMinVal] = useState(min);
-    const [maxVal, setMaxVal] = useState(max);
-    const minValRef = useRef(min);
-    const maxValRef = useRef(max);
-    const range = useRef<HTMLDivElement>(null);
+   const [minValue, setMinValue] = useState(min); // Initialize with min value
+   const [maxValue, setMaxValue] = useState(max); // Initialize with max value
+   const sliderRef = useRef<HTMLInputElement>(null);
+
+   
+   useEffect(() => {
+    setMinValue(min); // Update min value when min prop changes
+    setMaxValue(max); // Update max value when max prop changes
+}, [min, max]);
 
 
-    // convert to percentage
-    const getPercent = useCallback(
-        (value: number): number => Math.round(((value - min) / (max - min)) * 100),
-        [min, max]
-    );
 
-    // set width of the range to decrease from the left side
-    useEffect(() => {
-        const minPercent = getPercent(minVal);
-        const maxPercent = getPercent(maxValRef.current);
-
-        if (range.current) {
-            range.current.style.left = `${minPercent}%`;
-            range.current.style.width = `${maxPercent - minPercent}%`;
+   const handleMinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(e.target.value);
+        if (value <= maxValue) {
+            setMinValue(value);
+            console.log('Min value:', value);
+            onChange({ min: minValue, max: maxValue });
+        } else {
+            setMinValue(maxValue-2);
+            console.log('Min value:', value);
+            onChange({ min: minValue, max: maxValue });
         }
-    }, [minVal, getPercent]);
+   };
 
-    // set the width of the range to decrease from right side
-    useEffect(() => {
-        const minPercent = getPercent(minValRef.current);
-        const maxPercent = getPercent(maxVal);
-
-        if (range.current) {
-            range.current.style.width = `${maxPercent - minPercent}%`;
+   const handleMaxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = parseInt(e.target.value);
+        if (maxValue <= minValue) {
+            setMaxValue(value + 2);
+            console.log('Max value:', value);
+            onChange({ min: minValue, max: maxValue });
+        } else if (value > max) {
+            setMaxValue(max);
+            onChange({ min: minValue, max: maxValue });
+        } else {
+            setMaxValue(value);
+            onChange({ min: minValue, max: maxValue });
         }
-    }, [maxVal, getPercent]);
+   };
 
-    // Get min and max values when their state changes
-    useEffect(() => {
-        if (minVal != minValRef.current || maxVal != maxValRef.current) {
-            onChange({ min: minVal, max: maxVal });
-            minValRef.current = minVal;
-            maxValRef.current = maxVal;
+   const handleMinInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = e.target.value.replace(currencyText, '');
+        const value = parseInt(inputValue);
+        console.log('Min Input value:', inputValue);
+        if (inputValue === '' || (!isNaN(value) && value >= min && value <= max)) {
+            setMinValue(inputValue === '' ? 0 : value);
+            
         }
-    }, [minVal, maxVal, onChange]);
+        if(inputValue === ''){
+            setMinValue(0);
+            onChange({ min: minValue, max: maxValue });
+        }
+   };
+
+   const handleMaxInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const inputValue = e.target.value.replace(currencyText, '');
+        const value = parseInt(inputValue);
+        if (inputValue === '' || (!isNaN(value) && value >= min && value <= max)) {
+            setMaxValue(inputValue === '' ? 0 : value);
+        } else if(value > max) {
+            setMaxValue(max);
+            e.target.value = max.toString();
+        } else {
+            handleMaxChange(e);
+        }
+   };
+
+   const getMinThumbPosition = () => {
+        const percentage = ((minValue - min) / (max - min)) * 100;
+        return percentage;
+   };
+
+   const getMaxThumbPosition = () => {
+        const percentage = ((maxValue - min) / (max - min)) * 100;
+        return percentage;
+   };
+
+
 
     return (
-        <div className='w-full flex items-center justify-center flex-col space-y-14'>
-
-            {/* Display Price Value */}
-            <div className="w-[300px] px-4 flex items-center justify-between gap-x-5">
-
-                <p className="text-xl text-neutral-100 font-semibold">
-                    {currencyText} {minVal}
-                </p>
-
-                <div className="flex-1 border-dashed border border-neutral-500 mt-1"></div>
-
-                <p className="text-xl text-neutral-100 font-semibold">
-                    {currencyText} {maxVal}
-                </p>
-
-            </div>
-
-
-            {/* Style the price range slider */}
-            <div className="multi-slide-input-container" style={{ width }}>
-
-                <input
-                    type="range"
-                    min={min}
-                    max={max}
-                    value={minVal}
-                    onChange={(event) => {
-                        const value = Math.min(Number(event.target.value), maxVal - 1);
-                        setMinVal(value);
-                    }}
-                    className="thumb thumb-left"
-                    style={{
-                        width,
-                        zIndex: minVal > max - 100 || minVal === maxVal ? 5 : undefined,
-                    }}
-                />
-
-                <input
-                    type="range"
-                    min={min}
-                    max={max}
-                    value={maxVal}
-                    onChange={(event) => {
-                        const value = Math.max(Number(event.target.value), minVal + 1);
-                        setMaxVal(value);
-                    }}
-                    className="thumb thumb-right"
-                    style={{
-                        width,
-                        zIndex: minVal > max - 100 || minVal === maxVal ? 4 : undefined,
-                    }}
-                />
-
-                <div className="slider">
-                    <div
-                        style={{ backgroundColor: trackColor }}
-                        className="track-slider"
+        <div>
+            <div>
+                <h3 className='mt-4 mb-2 font-semibold text-slate-600'>Cijena</h3>
+                <div className='relative '>
+                    <input 
+                        type="range" 
+                        min={min} 
+                        max={max} 
+                        value={minValue}
+                        onChange={handleMinChange} 
+                        className='absolute w-full '
+                        style={{
+                            background: `linear-gradient(to right, #ddd ${getMinThumbPosition()}%, #bd244a ${getMinThumbPosition()}%, #bd244a ${getMaxThumbPosition()}%, #ddd ${getMaxThumbPosition()}%)`
+                        }}
+                        ref={sliderRef} // Attach the reference to the slider
                     />
-
-                    <div
-                        ref={range}
-                        style={{ backgroundColor: rangeColor }}
-                        className="range-slider"
+                    <input 
+                        type="range" 
+                        min={min} 
+                        max={max} 
+                        value={maxValue}
+                        onChange={handleMaxChange} 
+                        className='absolute w-full '
+                        style={{
+                            background: 'none'
+                        }}
                     />
-
                 </div>
-
             </div>
-
+            <div className='flex justify-between'>
+                <div>
+                    <h3 className='mt-4 mb-2 font-semibold text-slate-600'>Minimalno</h3>
+                    <input 
+                        type="text" 
+                        value={`${currencyText}${minValue}`} // Use minValue for input value
+                        onChange={handleMinInputChange} // Handle input change
+                        className='border-2 border-slate-950 rounded-xl w-20 h-12 text-center placeholder-black focus:border-black focus:border-4 outline-none transition-all duration-75'
+                    />
+                </div>
+                <div>
+                    <h3 className='mt-4 mb-2 font-semibold text-slate-600'>Maksimalno</h3>
+                    <input 
+                        type="text" 
+                        value={`${currencyText}${maxValue}`} // Use maxValue for input value
+                        onChange={handleMaxInputChange} // Handle input change
+                        className='border-2 border-slate-950 rounded-xl w-20 h-12 text-center placeholder-black focus:border-black focus:border-4 outline-none transition-all duration-75'
+                    />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Slider;
